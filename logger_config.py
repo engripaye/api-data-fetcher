ad2751c3f6b7f1d8c7d978e36e63bc4fddacc3e0
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-02 10:10:21 +0100

    backend code commit by ENGR. IPAYE

--- logger_config.py
+++ logger_config.py
@@ -4,3 +4,13 @@ from logging.handlers import RotatingFileHandler
 
 def setup_logger():
     logger = logging.getLogger("api-fetcher")
+    logger.setLevel(logging.INFO)
+
+    handler = RotatingFileHandler("fetcher.log", maxBytes=2000000, backupCount=3)
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    if not logger.hasHandlers():
+        logger.addHandler(handler)
+
+    return logger
